2c592aa6e695be5c301f2689bb3f06748704202b
Efosa Igbinehi <whizkidefos@users.noreply.github.com>
2023-10-14 17:47:32 +0100

    Building Header and Socials

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,6 +1,29 @@
+import Image from "next/image";
+import Link from "next/link";
+
+import Socials from "../components/Socials";
+
 const Header = () => {
   return (
-    <header className="absolute z-30 w-full">Header</header>
+    <header className="absolute z-30 flex items-center w-full px-16 xl:px-0 xl:h-[90px]">
+      <div className="container mx-auto">
+        <div className="flex flex-col items-center justify-between py-8 lg:flex-row gap-y-6">
+          {/* Logo */}
+          <Link href="/">
+            <Image
+              src={'/logo.svg'}
+              alt="logo"
+              width={200}
+              height={50}
+              className="cursor-pointer"
+              priority={true}
+            />
+          </Link>
+          {/* Socials */}
+          <Socials />
+        </div>
+      </div>
+    </header>
   );
 };
 
--- components/Socials.jsx
+++ components/Socials.jsx
@@ -1,5 +1,17 @@
+import Link from "next/link";
+import { RiFacebookLine, RiTwitterLine, RiInstagramLine, RiBehanceLine, RiDribbbleLine, RiPinterestLine } from 'react-icons/ri';
+
 const Socials = () => {
-  return <div>Socials</div>;
+  return (
+    <div className="flex items-center text-lg gap-x-5">
+      <Link href="/" target="_blank" className="transition-all duration-300 hover:text-accent"><RiBehanceLine /></Link>
+      <Link href="/" target="_blank" className="transition-all duration-300 hover:text-accent"><RiFacebookLine /></Link>
+      <Link href="/" target="_blank" className="transition-all duration-300 hover:text-accent"><RiTwitterLine /></Link>
+      <Link href="/" target="_blank" className="transition-all duration-300 hover:text-accent"><RiInstagramLine /></Link>
+      <Link href="/" target="_blank" className="transition-all duration-300 hover:text-accent"><RiDribbbleLine /></Link>
+      <Link href="/" target="_blank" className="transition-all duration-300 hover:text-accent"><RiPinterestLine /></Link>
+    </div>
+  );
 };
 
 export default Socials;
